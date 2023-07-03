dbc2c313ae729187fa178329ffbb1505db3f081e
Lennart401 <lennart.popkes@gmail.com>
2023-07-03 23:52:12 +0200

    Refactor for different model uses

--- paper_code.py
+++ paper_code.py
@@ -2,28 +2,32 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pathlib
 
 from tqdm import tqdm
 from scipy.stats import bernoulli, binom, dirichlet, beta, multinomial, norm, lognorm, uniform
 from datetime import datetime
 
+RUN_NAME = pathlib.Path(__file__).name[:-3]
 TINY_CONST = 1e-10
 SHOW_PLOTS = False
 SAVE_PLOTS = True
 START_TIME = datetime.now().strftime("%Y%m%d-%H%M%S")
-PLOTS_PATH = f'plots/{START_TIME}'
+PLOTS_PATH = f'plots/{RUN_NAME}-{START_TIME}'
 PLOT_SIZE = (22, 9)
 PLOT_DPI = 600
 SAVE_RESULTS = True
-RESULTS_PATH = f'results/{START_TIME}'
+RESULTS_PATH = f'results/{RUN_NAME}-{START_TIME}'
+USE_ORIGINAL_MODEL = True
 USE_EXTENSION_LAMBDA = False
 USE_EXTENSION_THETA = False
-USE_REAL_DATA = True
+USE_REAL_DATA = False
+SIMULATION_DATA = 'simulation_data/data.npz'
 
 # Script length variables
 N_EXAMINEES = 500
-N_ITERATIONS = 10000
-N_REPEATS = 1
+N_ITERATIONS = 3000
+N_REPEATS = 10
 
 if int(os.environ.get('USE_TEST_MODE', 0)) == 1:
     print('Running in test mode...')
@@ -31,8 +35,8 @@ if int(os.environ.get('USE_TEST_MODE', 0)) == 1:
     SAVE_PLOTS = False
     SAVE_RESULTS = False
     PLOT_DPI = 100
-    N_EXAMINEES = 250
-    N_ITERATIONS = 100
+    N_EXAMINEES = 100
+    N_ITERATIONS = 30
     N_REPEATS = 1
 
 if SAVE_PLOTS:
@@ -48,15 +52,20 @@ plt.rcParams["figure.dpi"] = PLOT_DPI
 
 # Simulation parameters
 mu_hyperparameter_1 = mu_hyperparameter_2 = 0.5
-pi_hyperparams = np.array([1, 1])
-pi_hyperparams_simulation = np.array([1, 1])
+pi_hyperparams = np.array([1, 1]) if not USE_ORIGINAL_MODEL else np.array([0.01, 0.01])
+pi_hyperparams_simulation = np.array([1, 1]) if not USE_ORIGINAL_MODEL else np.array([0.01, 0.01])
 
 true_slipping = 0.3
 true_guessing = 0.1
 
 theta_weight = 0.7
 
-strategy_a_q_matrix = np.array([
+# ---------------------------------------------------------------------------------------------------------------------
+# Q MATRICES
+# ---------------------------------------------------------------------------------------------------------------------
+
+# DE LA TORRE 2008
+de_la_torre_strategy_a_q_matrix = np.array([
     [1, 1, 1, 1, 0],
     [1, 1, 1, 1, 0],
     [1, 0, 0, 0, 1],
@@ -76,10 +85,10 @@ strategy_a_q_matrix = np.array([
     [0, 1, 1, 1, 0],
     [0, 1, 1, 0, 1],
     [0, 1, 0, 1, 1],
-    [0, 0, 1, 1, 1]
+    [0, 0, 1, 1, 1],
 ])
 
-strategy_b_q_matrix = np.array([
+de_la_torre_strategy_b_q_matrix = np.array([
     [0, 0, 0, 0, 1],
     [0, 0, 0, 0, 1],
     [0, 1, 0, 0, 0],
@@ -99,7 +108,161 @@ strategy_b_q_matrix = np.array([
     [0, 0, 0, 0, 1],
     [1, 1, 0, 0, 0],
     [1, 0, 0, 1, 0],
-    [1, 0, 0, 1, 0]
+    [1, 0, 0, 1, 0],
+])
+
+# SIMULATION STUDY 2
+s2_a_q_matrix = np.array([
+    [1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+    [0, 1, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 0, 1],
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1],
+    [1, 1, 1, 0, 0],
+    [1, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1],
+    [1, 0, 1, 1, 0],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 1],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 1],
+    [0, 1, 0, 1, 1],
+    [0, 0, 1, 1, 1],
+    [1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+    [0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+])
+
+s2_b_q_matrix = np.array([
+    [0, 1, 0, 1, 1],
+    [0, 0, 1, 1, 1],
+    [0, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 1, 1],
+    [1, 1, 0, 0, 1],
+    [1, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+    [0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+])
+
+# SIMULATION STUDY 3
+s3_b_q_matrix = np.array([
+    [1, 1, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1],
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 1],
+])
+
+s3_a_q_matrix = np.array([
+    [0, 0, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1],
+    [0, 1, 0, 0, 1],
+    [0, 0, 1, 1, 0],
+    [1, 0, 0, 1, 1],
+    [1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 1, 1],
+    [0, 1, 0, 0, 1],
+    [1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [1, 1, 0, 0, 1],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
 ])
 
 if USE_REAL_DATA:
@@ -110,7 +273,18 @@ if USE_REAL_DATA:
     q = np.stack([q_a, q_b]).transpose([1, 2, 0])
 
 else:
-    q = np.stack([strategy_a_q_matrix, strategy_b_q_matrix]).transpose([1, 2, 0])
+    # de la Torre 2008
+    q = np.stack([de_la_torre_strategy_a_q_matrix, de_la_torre_strategy_b_q_matrix]).transpose([1, 2, 0])
+
+    # Simulation Study 2
+    # q = np.stack([s2_a_q_matrix, s2_b_q_matrix]).transpose([1, 2, 0])
+
+    # Simulation Study 3
+    # q = np.stack([s3_a_q_matrix, s3_b_q_matrix]).transpose([1, 2, 0])
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SIMULATION PARAMETERS
+# ---------------------------------------------------------------------------------------------------------------------
 
 n_attributes = q.shape[1]  # K attributes
 n_strategies = q.shape[2]  # M strategies
@@ -124,35 +298,56 @@ pi_avg = np.ones(N_REPEATS)
 # ---------------------------------------------------------------------------------------------------------------------
 # SIMULATION THE EXAMINEES TAKING A TEST
 # ---------------------------------------------------------------------------------------------------------------------
-if not USE_REAL_DATA:
-    alpha_sim = np.array(bernoulli.rvs(0.5, size=(n_examinees, n_attributes)))
-    pi = 1 - dirichlet.rvs(pi_hyperparams_simulation, size=1).flatten()
-    pi_true = pi
-    s_c = np.ones((n_items, n_strategies)) * (1 - true_slipping)
-    g = np.ones((n_items, n_strategies)) * true_guessing
-
-    print('pi for simulation:', pi)
-
-    # build score from item response function
-    eta = np.zeros(shape=(n_examinees, n_items, n_strategies))
-    for i in range(n_examinees):
-        for j in range(n_items):
-            for m in range(n_strategies):
-                eta[i, j, m] = np.prod([alpha_sim[i, k] ** q[j, k, m] for k in range(n_attributes)])
-
-    score = np.zeros(shape=(n_examinees, n_items))
-    for i in range(n_examinees):
-        for j in range(n_items):
-            score[i, j] = bernoulli.rvs(
-                np.sum([pi[m] * s_c[j, m] ** eta[i, j, m] * g[j, m] ** (1 - eta[i, j, m]) for m in range(n_strategies)]))
 
 if USE_REAL_DATA:
     score = pd.read_csv('fs_score.csv', header=0, index_col=0).values
     n_examinees = score.shape[0]
 
+else:
+    if SIMULATION_DATA is not None and pathlib.Path(SIMULATION_DATA).exists():
+        # load simulation data if it already exists
+        print(f'Loading simulation data from {SIMULATION_DATA}...')
+        saved_data = np.load(SIMULATION_DATA)
+        alpha_sim = saved_data['alpha_sim']
+        pi = saved_data['pi']
+        pi_true = saved_data['pi']
+        s_c = saved_data['s_c']
+        g = saved_data['g']
+        eta = saved_data['eta']
+        score = saved_data['score']
+
+    else:
+        alpha_sim = np.array(bernoulli.rvs(0.5, size=(n_examinees, n_attributes)))
+        pi = 1 - dirichlet.rvs(pi_hyperparams_simulation, size=1).flatten()
+        pi_true = pi
+        s_c = np.ones((n_items, n_strategies)) * (1 - true_slipping)
+        g = np.ones((n_items, n_strategies)) * true_guessing
+
+        print('pi for simulation:', pi)
+
+        # build score from item response function
+        eta = np.zeros(shape=(n_examinees, n_items, n_strategies))
+        for i in range(n_examinees):
+            for j in range(n_items):
+                for m in range(n_strategies):
+                    eta[i, j, m] = np.prod([alpha_sim[i, k] ** q[j, k, m] for k in range(n_attributes)])
+
+        score = np.zeros(shape=(n_examinees, n_items))
+        for i in range(n_examinees):
+            for j in range(n_items):
+                score[i, j] = bernoulli.rvs(
+                    np.sum([pi[m] * s_c[j, m] ** eta[i, j, m] * g[j, m] ** (1 - eta[i, j, m]) for m in range(n_strategies)]))
+
+        if SIMULATION_DATA is not None:
+            # save simulation data
+            print(f'Saving simulation data to {SIMULATION_DATA}...')
+            os.makedirs(pathlib.Path(SIMULATION_DATA).parent.resolve())
+            np.savez(SIMULATION_DATA, alpha_sim=alpha_sim, pi=pi, s_c=s_c, g=g, eta=eta, score=score)
+
 # ---------------------------------------------------------------------------------------------------------------------
 # START WITH MCMC SAMPLING
 # ---------------------------------------------------------------------------------------------------------------------
+
 EM = N_ITERATIONS
 BI = int(EM / 2)
 repetitions = N_REPEATS  # number of repetitions
@@ -194,8 +389,9 @@ for rep in range(repetitions):
         lambda_1 = np.array([1.34, 1.22, 1.08, 1.11, 0.97])
 
     # Start the MCMC
-    print('\nRepition: ', rep)
+    print('\nRepetition: ', rep)
     for WWW in tqdm(range(EM)):
+
         # -------------------------------------------------------------------------------------------------------------
         # GIBBS SAMPLING
         # -------------------------------------------------------------------------------------------------------------
@@ -203,7 +399,10 @@ for rep in range(repetitions):
         # draw pi using Gibbs Sampler (always accept, draw from conditional posterior)
         membership_counts = np.array([np.sum(c == m) for m in range(n_strategies)])
         # membership_counts = np.flip(membership_counts)
-        pi = dirichlet.rvs(pi_hyperparams + membership_counts, size=1).flatten()
+        if USE_ORIGINAL_MODEL:
+            pi = 1 - dirichlet.rvs(pi_hyperparams + membership_counts, size=1).flatten()
+        else:
+            pi = dirichlet.rvs(pi_hyperparams + membership_counts, size=1).flatten()
         pi_hat[rep, WWW] = pi
 
         # draw c (strategy membership parameter), using Gibbs Sampler (always accept, draw from conditional posterior)
@@ -240,11 +439,16 @@ for rep in range(repetitions):
         num_attributes = np.sum(alpha, axis=1)
         attr_sum = [np.sum(num_attributes[c == m]) for m in range(n_strategies)]
 
-        mu = [
-            np.random.beta(attr_sum[m] + mu_hyperparameter_1, alpha[c == m].size - attr_sum[m] + mu_hyperparameter_2)
-            # np.random.beta(attr_sum[m] + mu_hyperparameter_1, alpha.size - attr_sum[m] + mu_hyperparameter_2)
-            for m in range(n_strategies)
-        ]
+        if USE_ORIGINAL_MODEL:
+            mu = [
+                np.random.beta(attr_sum[m] + mu_hyperparameter_1, alpha.size - attr_sum[m] + mu_hyperparameter_2)
+                for m in range(n_strategies)
+            ]
+        else:
+            mu = [
+                np.random.beta(attr_sum[m] + mu_hyperparameter_1, alpha[c == m].size - attr_sum[m] + mu_hyperparameter_2)
+                for m in range(n_strategies)
+            ]
         mu_hat[rep, WWW] = mu
 
         # -------------------------------------------------------------------------------------------------------------
@@ -320,8 +524,6 @@ for rep in range(repetitions):
             # Ratio of likelihoods for the new and prior alpha values
             LLa = binom.pmf(np.sum(alpha_new), n_attributes, probability) / \
                   binom.pmf(np.sum(alpha[examinee, :]), n_attributes, probability)
-            # LLa = binom.pmf(np.sum(alpha[examinee, :]), n_attributes, mu[strategy_membership]) / \
-            #       binom.pmf(np.sum(alpha_new), n_attributes, mu[strategy_membership])
 
             LLLa = 1
             # TODO parallelize / vectorize
@@ -336,10 +538,8 @@ for rep in range(repetitions):
 
                 if score[examinee, item] == 1:
                     temp = tem_new / np.maximum(tem, TINY_CONST)
-                    # temp = np.maximum(tem, TINY_CONST) / tem_new
                 else:
                     temp = (1 - tem_new) / (1 - tem)
-                    # temp = (1 - tem ) / (1 - tem_new)
 
                 LLLa *= temp
 
@@ -357,12 +557,9 @@ for rep in range(repetitions):
             for strategy in range(n_strategies):
                 g_new[item, strategy] = np.random.uniform(0.0, 0.2)
                 s_c_new[item, strategy] = np.random.uniform(0.6, 0.8)
-                # g_new[item, strategy] = beta.rvs(1, 2) * 0.4 + 0.1
-                # s_c_new[item, strategy] = beta.rvs(1, 2) * 0.4 + 0.1
 
             likelihood = np.ones(n_strategies)
 
-            # { PROD i=1 to N: [ p_ijm ^ u_ij * (1 - p_ijm) ^ (1 - u_ij) ] ^ c_j ] ^ I(c_i = m) } Beta(s_jm) x Beta(g_jm)
             # TODO parallelize / vectorize
             for examinee in range(n_examinees):
                 strategy_membership = int(c[examinee])
@@ -571,16 +768,21 @@ for rep in range(repetitions):
         plt.close()
 
     # score and predicted probability
-    fig, ax = plt.subplots(nrows=3, ncols=1)
+    score_diff = np.abs(score - np.round(score_pred))
+
+    fig, ax = plt.subplots(nrows=4, ncols=1)
     score_ax = ax[0].matshow(score.T, aspect='auto', cmap=cmap)
     ax[0].set_title('score')
     score_pred_ax = ax[1].matshow(score_pred.T, aspect='auto', cmap=cmap)
     ax[1].set_title('score_pred')
     p_MMS_ax = ax[2].matshow(p_MMS.T, aspect='auto', cmap=cmap)
     ax[2].set_title('p_MMS')
+    score_diff_ax = ax[3].matshow(1 - score_diff.T, aspect='auto', cmap='PiYG')
+    ax[3].set_title('score - score_pred')
     fig.colorbar(score_ax, location=colorbar_location)
     fig.colorbar(score_pred_ax, location=colorbar_location)
     fig.colorbar(p_MMS_ax, location=colorbar_location)
+    fig.colorbar(score_diff_ax, location=colorbar_location)
     plt.suptitle(f'score vs. p_MMS ({rep})')
     plt.tight_layout()
     if SAVE_PLOTS:
@@ -590,11 +792,32 @@ for rep in range(repetitions):
     plt.clf()
     plt.close()
 
-    # Theta values
-    print(theta)
+    # -----------------------------------------------------------------------------------------------------------------
+    # SAVE RESULTS
+    # -----------------------------------------------------------------------------------------------------------------
 
     if SAVE_RESULTS:
-        np.savez(f'{RESULTS_PATH}/{rep}.npz', alpha=alpha, c=c, pi=pi, mu=mu, s_c=s_c, g=g)
+        np.savez(
+            f'{RESULTS_PATH}/{rep}.npz',
+            c_hat=c_hat[rep],
+            pi_hat=pi_hat[rep],
+            mu_hat=mu_hat[rep],
+            slipping=slipping[rep],
+            guessing=guessing[rep],
+            alpha_hat=alpha_hat[rep],
+            theta_hat=theta_hat[rep],
+            lambda_0_hat=lambda_0_hat[rep],
+            lambda_1_hat=lambda_1_hat[rep],
+            score=score,
+            score_pred=score_pred,
+            score_diff=score_diff,
+            p_MMS=p_MMS,
+            alpha_sim=alpha_sim,
+            alpha_final=alpha_final,
+            alpha_diff=alpha_diff,
+            slipping_trace=slipping_trace[rep],
+            guessing_trace=guessing_trace[rep],
+        )
 
     # Keep track of avg values over repetitions
     pi_avg[rep] = pi_mean[0]
@@ -602,7 +825,7 @@ for rep in range(repetitions):
     guessing_trace_avg[rep] = np.mean(guessing_trace, axis=0)
 
 # -----------------------------------------------------------------------------------------------------------------
-# Error Measures
+# ERROR MEASURES
 # -----------------------------------------------------------------------------------------------------------------
 
 # Bias
@@ -678,6 +901,14 @@ if not USE_REAL_DATA:
 
     classification_rate = counter / n_examinees
     print(f'proportion of examinees classified incorrectly for at least K-1 attributes: {classification_rate}')
+
+
+# Trace plots of multiple chains
+if repetitions > 1:
+    pass
+
+# Density plots
+pass
 
 # p_MMS_log[:, :, rep] = np.log(p_MMS)
 # logLik_MMS[rep] = np.sum(np.sum(np.log(p_MMS)))
